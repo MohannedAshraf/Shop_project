7f53ccfdf34149162de3dd73a3923604830610d8
mohanned <MohannedAshraf GitHub>
2025-03-23 20:53:31 +0200

    last edit

--- lib/feature/profile/view/my_profile_view.dart
+++ lib/feature/profile/view/my_profile_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:ntigradproject/core/resources_manager/default-appbar.dart';
 import 'package:ntigradproject/core/utils/app-colors.dart';
 import 'package:ntigradproject/core/utils/app_icons.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ntigradproject/feature/auth/manager/login_cubit/login_cubit.dart';
-import 'package:ntigradproject/feature/home/view/home_view.dart';
 
-import 'package:ntigradproject/feature/profile/view/main_profile.dart';
-import 'package:ntigradproject/feature/profile/view/my_profile_view.dart';
 import 'package:ntigradproject/feature/profile/view/settings_view.dart';
 
 void main() {
